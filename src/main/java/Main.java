91785a11e3e906588619c168b9e5fce1339ddf8c
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-09-02 12:23:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("- Breaking Bad -");
+    System.out.println("");
+    System.out.println("Breaking Bad segue a história de Walter White, um professor de química que se torna um produtor e traficante de metanfetamina após ser diagnosticado com câncer. Ele se une a um ex-aluno, Jesse Pinkman, e juntos mergulham no submundo do crime. ");
+    System.out.println("");
+    System.out.println("Breaking Bad é uma série de Drama Criminal");
+    System.out.println("");
+    System.out.println("link do trailer:");
+    System.out.println("");
+System.out.println("https://youtu.be/HhesaQXLuRY?si=BFVwML9LgnZfmFRL");
+  }
+}
